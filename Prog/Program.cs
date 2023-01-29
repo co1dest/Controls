6d65bb6a716190c30ca4ad4b4023bd387bbdaf2d
Boronin Alexander <co1dest@yandex.ru>
2023-01-30 02:41:50 +0300

    Создал консоль и написал программу

---
+++ Prog/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+
+Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа
+Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма
+При решении не рекомендуеться пользоваться коллекциями, лучше обойтись исключительно массивами
+
+*/
+
+Console.Write("Введите массив данных через ',': ");
+string strLine = Console.ReadLine();
+string[] inArr = strLine.Split(",");
+string[] outArr = new string[0];
+
+for (int i = 0; i < inArr.Length; i++)
+{
+if (inArr[i].Length <= 4)
+{
+Array.Resize(ref outArr, outArr.Length + 1);
+outArr[outArr.Length-1] = inArr[i];
+}
+
+}
+Console.WriteLine($"[{string.Join(",", inArr)}]=>[{string.Join(",", outArr)}]");
